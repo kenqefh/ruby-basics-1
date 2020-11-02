8219c93f4e96dd8104723531c1e52ed32c923f79
Elvis Frank Dominguez Vidal <elvisf.dominguez@gmail.com>
2020-11-02 13:43:58 -0500

    Finished multiplier

--- 03multiplier/multiplier.rb
+++ 03multiplier/multiplier.rb
@@ -9,3 +9,20 @@
 
 # Use some looping technique to print each multiple followed by a comma.
 # The last one should be preceded by "and"
+
+puts "My name is Multiplier and I will give you the first 20 multiples of any number"
+
+puts "Choose a number greater than 0:"
+user_number = gets.chomp.to_i
+
+# if value is invalid
+user_number = 5 if user_number.zero?
+
+# Saving data
+result = "#{user_number},"
+2.upto(19) { |x| result << " #{x * user_number}," }
+result << " and #{20 * user_number}"
+
+# Showing result
+puts "The first 20 multiples of #{user_number} are:"
+puts result
