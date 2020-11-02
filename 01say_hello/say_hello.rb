72454aad21e9334488004e4bc44ec3f8aa29d9e0
Elvis Frank Dominguez Vidal <elvisf.dominguez@gmail.com>
2020-11-02 12:05:55 -0500

    finished say hello challenge, now you can try it

--- 01say_hello/say_hello.rb
+++ 01say_hello/say_hello.rb
@@ -2,3 +2,12 @@
 
 # If the user enters some text like "Monica", print "Hello Monica! Welcome to the Ruby module."
 # If the user enters nothing, print "No one to say hello to. :cry:"
+
+puts "Hi mate!, What's your name?"
+name = gets.chomp
+
+if name.empty?
+  puts "No one to say hello to. :cry:"
+else
+  puts "Hello #{name}! Welcome to the Ruby module."
+end
