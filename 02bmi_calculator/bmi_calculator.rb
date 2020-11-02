2e4e5ba6ad300c7dcfe02b24c32816625415aa86
Elvis Frank Dominguez Vidal <elvisf.dominguez@gmail.com>
2020-11-02 13:14:10 -0500

    BMI calculator works well, it pass every tests

--- 02bmi_calculator/bmi_calculator.rb
+++ 02bmi_calculator/bmi_calculator.rb
@@ -5,3 +5,21 @@
 # Calculate the BMI and print the message with the result
 
 # If the result is less than 18.5 print "You are underweight, add more potato to the broth."
+
+puts "How much do you weigh?"
+weight = gets.chomp.to_i
+
+puts "How tall are you?"
+height = gets.chomp.to_f
+
+BMI = (weight / (height**2)).round(2)
+
+puts "Right now your BMI is #{BMI}"
+
+if BMI < 18.5
+  puts "You are underweight, add more potato to the broth"
+elsif BMI >= 18.5 && BMI < 25
+  puts "You have a normal weight, I have healthy envy of you"
+else
+  puts "You are overweight, I know, the pandemic has affected us all"
+end
